c4639241edc0cf2dd81379ebed63b8b84305db6c
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-04-29 21:20:48 +0200

    Update snake.js

---
+++ snake.js
@@ -0,0 +1,43 @@
+
+class Snake {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.tail = [{x: this.x, y: this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
+    }
+
+    move() {
+        var newRect;
+        if(this.rotateX == 1) {
+            newRect = {
+                x: this.tail[this.tail.length - 1].x + this.size,
+                y: this.tail[this.tail.length - 1].y
+            }
+        }
+    }
+}
+
+
+var canvas = document.getElementById("canvas")
+
+var snake = new Snake();
+
+var apple = new Apple();
+
+var canvasContext = canvas.getContext('2d');
+
+window.onload = () => {
+    gameLoop();
+}
+
+function gameLoop() {
+    setInterval(show, 1000/15)
+}
+
+function show() {
+    update();
+    draw();
+}
